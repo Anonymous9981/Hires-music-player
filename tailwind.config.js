a8570d080c65265fd0fc87dbdda9fa5e8516d0b0
Yashwant Mukati <yashwantid9981@gmail.com>
2025-07-30 07:10:36 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // This line enables dark mode based on a class in the HTML
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
